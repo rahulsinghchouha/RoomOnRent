ddd1c9fcef92ac036c033a97c44e8a92b8b1ecf4
rahulsinghchouha <singhrahul15737@gmail.com>
2024-07-07 13:21:45 +0530

    build cmd added

---
+++ node.app/controller/productController.js
@@ -0,0 +1,111 @@
+const Products = require("../model/Products");
+
+
+module.exports.search = async (req,res)=>{
+
+    console.log(req.query);
+    
+    const latitude = req.query.loc.split(',')[0];//split krne se ye array ho jayega jiska 0 index hm le lenge then
+    const longitude = req.query.loc.split(',')[1];//then yaha se hm longitude nikal lenge 
+
+    //is query ke basis pr hm result ko find out krenge
+    let search = req.query.search;
+    //find the data from mongodb is prakar hm mongodb se data ko find krenge
+    Products.find({
+        $or:[
+           { productName:{$regex:search} },
+           { productDesc:{$regex:search} },
+           { category:{$regex:search} },
+           { price:{$regex:search} },
+        ],
+        //hmara second criteria jo hoga vo location vala hoga
+        // pLoc:{//hm near vale bhi return krke denge
+        //     $near:{
+        //         $geometry: {
+        //             type:"Point",
+        //             coordinates:[parseFloat(latitude),parseFloat(longitude)]
+        //         }
+        //     },
+        //     $maxDistance :500 * 1000,
+        // }
+    })
+    .then((result)=>{
+        console.log("user data ->",result)
+        res.send({message:'data fetch succesfully',products:result})})
+    .catch((err)=>{res.send({message:'server err'})})
+
+}
+
+module.exports.addProduct = async(req,res)=>{
+    //for check what i get from body
+   
+    console.log(req.files); //more than one so we use files
+    console.log(req.body);
+
+    //fetching the data 
+    const plat = req.body.plat;
+    const plong = req.body.plong;
+    const {productName,productDesc,price,category} = req.body;
+    const productImage = req.files.productImage[0].path;  //yaha hm product ki image nikal rahe hai jo ki product ki form men hai
+    const productImage2 = req.files.productImage2[0].path;
+    const addedBy = req.body.userId;
+
+    //entry in database key value same to ek hee bar likh sakte hai
+
+    const products = new Products ({
+        productName,productDesc,
+        price,category,productImage,
+        productImage2,addedBy,
+       pLoc:{type:'Point', coordinates:[plat,plong]}
+});
+
+    await products.save()
+    .then(()=>{res.send({message : 'saved success'})})
+    .catch((err)=>{res.send({message: 'server err'})})
+}
+
+module.exports.getProduct = async(req,res)=>{
+
+    const catName = req.query.catName;
+
+    //console.log("category--->",catName);
+
+    let _f = {}
+    if(catName)
+        {
+            _f={category:catName}
+        }
+
+    await Products.find(_f)
+    .then((result)=>{
+        console.log("user data ->",result)
+        res.send({message:'data fetch succesfully',products:result})})
+    .catch((err)=>{res.send({message:'server err'})})
+
+}
+
+module.exports.getProductById = async(req,res)=>{
+    //we can check the data using the console
+    console.log(req.params);
+    //ye id find out krenge jo ki product ki id hai
+    //ek hee product hoga isliye findOne ka use krenge
+    await Products.findOne({_id:req.params.productId})
+    .then((result)=>{
+        console.log("user data ->",result)
+        res.send({message:'data fetch succesfully',product:result})})
+    .catch((err)=>{res.send({message:'server err'})})
+}
+
+module.exports.myProducts = async (req,res)=>{
+    //liked products ko hm user ke andar se find krenge
+    //console.log("liked products")
+    const userId = req.body.userId;
+    //user id added by ke equal hai vo vale product
+    await Products.find({addedBy:userId})
+    .then((result)=>{
+    console.log("user data ->",result)
+    res.send({message:'data fetch succesfully',products:result})})
+    .catch((err)=>{res.send({message:'server err',err})})
+}
+
+
---
+++ node.app/controller/userController.js
@@ -0,0 +1,138 @@
+const User = require("../model/User")
+const jwt = require('jsonwebtoken');
+
+module.exports.likeProducts = async(req,res)=>{
+    let productId = req.body.productId;
+    let userId = req.body.userId;
+    //console.log(req.body);
+    
+
+    //ab hme user vale model ko update krna hai liked array ko jese hm sql men where ka use 
+    // vese hee yaha bhi use krenge id user id ke equal honi chahiye
+    //yaha pr hmara like name ka array hai to usme data add krne ke liye hm $ addToSet ka use krenge
+    //likedProduct is schema name and productId we getch from body
+    await User.updateOne({_id:userId},
+                    {$addToSet :{likedProducts:productId}}
+                    )
+        .then(()=>{
+            res.send({message:"like success"})
+        })
+        .catch((err)=>{
+            res.send({message:"like server err"})
+        })
+
+}
+
+module.exports.signUp = async (req,res)=>{
+    //getting data from frontend
+
+    console.log(req.body);
+    const username = req.body.username;
+    const password = req.body.password;
+    const mobile = req.body.mobile;
+    const email = req.body.email;
+    const user = new User({username: username, password:password,email,mobile});
+
+    await user.save()
+     .then(()=>{
+        res.send({message:"saved succesfully"});
+     })
+     .catch((err)=>{
+        res.send({
+            message:"server err",
+            Error:err
+        })
+     })
+
+}
+
+module.exports.login = async (req,res)=>{
+    console.log(req.body);
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //isme hm find krenge
+     await User.findOne({username:username})
+    .then((result)=>{
+        console.log("user data",result); // comsole pr 
+        if(!result)
+            {
+                res.send({message:"user not found"}); //response sign vale handle api ke alert men print hoga
+            }
+            else{
+                if(result.password !== password)
+                    {
+                        res.send({message:"Password not match"});
+                    }
+                    else{
+                        //also retun a json web token
+                        const token = jwt.sign({
+                            data:result,    
+                        },
+                        'MYSECRETKEY',
+                        {
+                        expiresIn:'24h'
+                        }
+                     )
+                     //token men response men userId ko bhi send kr diya hai 
+                        res.send({message:"find succesfully",token:token,userId:result._id}); //message
+                    }
+            }
+       
+     })
+     .catch((err)=>{
+        res.send({
+            message:"server err",
+            Error:err
+        })
+     })
+}
+
+module.exports.myProfileById = async(req,res)=>{
+   
+    const uid = req.params.userId;
+
+    User.findOne({_id:uid})
+    .then((result)=>{
+        res.send({
+            message:" data fetched succesfully ",
+            user:{
+                email:result.email,
+                mobile:result.mobile,
+                username:result.username
+            }
+        })
+    })
+    .catch((err)=>{  
+        res.send({
+            message:"server err",
+            error:err
+        })
+    })
+}
+
+module.exports.getUserById = async(req,res)=>{
+    const _userId = req.params.uId;
+    User.findOne({_id:_userId})
+    .then((result)=>{
+        res.send({message:'success',user:{email:result.email,
+            mobile:result.mobile,
+            username:result.username}})
+    })
+    .catch((err)=>{
+        res.send({message:'saved error',
+                error:err
+        })
+    })
+}
+module.exports.likedProducts = async (req,res)=>{
+    //liked products ko hm user ke andar se find krenge
+    console.log("liked products")
+
+    await User.findOne({_id:req.body.userId}).populate('likedProducts')
+    .then((result)=>{
+    console.log("user data ->",result)
+    res.send({message:'data fetch succesfully',products:result.likedProducts})})
+    .catch((err)=>{res.send({message:'server err'})})
+
+}
